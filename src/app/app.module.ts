8e13694bc3662388997ff419a26c7a3885058716
Jon Moore <jonrmoore@protonmail.com>
2020-03-28 15:40:26 -0500

    restructuring components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,23 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { ShoppingListAdd } from "./shopping-list/add/add.component";
-import { ShoppingListEdit } from "./shopping-list/edit/edit.component";
-import { RecipeBookComponent } from "./recipe-book/recipe-book.component";
-import { RecipeList } from "./recipe-book/list/list.componnent";
-import { RecipeItem } from "./recipe-book/item/item.component";
-import { RecipeDetail } from "./recipe-book/detail/detail.component";
 import { HeaderComponent } from "./header/header.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { RecipeDetailsComponent } from "./recipes/recipe-details/recipe-details.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingListComponent,
-    ShoppingListAdd,
-    ShoppingListEdit,
-    RecipeList,
-    RecipeItem,
-    RecipeDetail,
-    RecipeBookComponent,
-    HeaderComponent
+    ShoppingEditComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    RecipeListComponent
   ],
   imports: [BrowserModule],
   providers: [],

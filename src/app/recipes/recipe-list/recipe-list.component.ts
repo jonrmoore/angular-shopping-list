994052ffa9035da1c6fb867c98c520ab68207c4c
Jon Moore <jonrmoore@protonmail.com>
2020-03-28 16:58:48 -0500

    dynamic rendering with recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.scss"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Test",
+      "This is a test",
+      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"
+    ),
+    new Recipe(
+      "again",
+      "This is another test",
+      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"
+    )
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

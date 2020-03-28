52158e7e6090008fcee03ec736f4b3a7e4bd4066
Jon Moore <jonrmoore@protonmail.com>
2020-03-28 13:51:51 -0500

    adding base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { ShoppingListAdd } from "./shopping-list/add/add.component";
+import { ShoppingListEdit } from "./shopping-list/edit/edit.component";
+import { RecipeBookComponent } from "./recipe-book/recipe-book.component";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListAdd,
+    ShoppingListEdit,
+    RecipeBookComponent,
+    HeaderComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shopping-list/add/add.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "shopping-list-add",
+  templateUrl: "./add.component.html",
+  styleUrls: ["./add.component.scss"]
+})
+export class ShoppingListAdd implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
